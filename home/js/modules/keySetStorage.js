539405b7390be05683b719e79dfcd71c830e1185
Davidev-wiki <accelerator@kakao.com>
2024-04-16 13:31:11 +0900

    edit path index.html

--- home/js/modules/keySetStorage.js
+++ home/js/modules/keySetStorage.js
@@ -10,30 +10,42 @@ buttons.forEach(button => {
 });
 
 function keySetting() {
-    list.innerHTML = ""; // 기존 리스트 초기화
 
-    for (let key in localStorage) {
-        const $li = document.createElement('li');
-        $li.textContent = `${key} : ${localStorage[key]}`;
-        console.log($li.textContent);
-        list.append($li);
+  const DISABLE_KEY = [
+    'loglevel',
+    'length',
+    'clear',
+    'getItem',
+    'key',
+    'removeItem',
+    'setItem'
+  ];
+
+  list.innerHTML = ""; // 기존 리스트 초기화
+  for (const key in localStorage) {
+    if (!DISABLE_KEY.includes(key) && localStorage.hasOwnProperty(key)) { // 키 체크
+      const $li = document.createElement('li');
+      $li.textContent = `${key} : ${localStorage[key]}`;
+      list.append($li);
     }
+  }
 }
 
 function editList(e) {
     keySet = document.getElementById('key').value;
     description = document.getElementById('description').value;
 
-    //const keyStrings = `${keySet} : ${description}`;
     const buttonFlag = e.target.id;
 
     switch (buttonFlag) {
         case "add":
             localStorage.setItem(keySet, description);
+            console.log('키가 등록되었어요');
             break;
         case "remove":
             localStorage.removeItem(keySet);
-            break;
+            console.log('키가 삭제되었어요');
+          break;
         default:
     }
     keySetting();
